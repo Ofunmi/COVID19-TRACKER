cb562d71c87ab2f1d3b84e427a58aebbee8d7dc0
Oluwafunmilayo Owolabi <funmiowolabi55@gmail.com>
2022-08-22 09:48:43 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Covid-19 Tracker</div>
+  </header>
+  <p>API by <a class="text-blu-300" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+ 
+}
+</script>
+
